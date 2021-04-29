23c1b0c6a698a0b0deb5a0d322b89d4abf8d8297
vpr-1 <grafikroz@ukr.net>
2021-04-29 18:23:07 +0300

    Initialize

---
+++ 22.04.2021/js/script.js
@@ -0,0 +1,12 @@
+// alert('Hello')
+// alert('world!')
+
+// [1, 2, 3, 4, "Привіт" ].forEach(alert)
+
+
+let message = 'Hello!';
+
+alert('message');
+// alert("Зараз буде помилка")
+
+// [1, 2].forEach(alert)
